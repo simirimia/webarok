b7ba76207b76d3940cc57b79679e91583ea0581d
simirimia <simirimia@triosolutions.at>
2015-03-07 12:06:15 +0100

    removed everything but webarok from this repository

--- webarok/build_from_current_revision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-mkdir build
-cd build
-svn co https://webarok.svn.sourceforge.net/svnroot/webarok/webarok/src webarok 
-find . -name ".svn" -exec rm -rf {} \;
-tar cjf webarok.tar.bz2 webarok
-rm -rf build
--- webarok/src/static/phone/theme.js
+++
@@ -1,5 +0,0 @@
-var theme = 'phone';
-
-var playlist_item_template_html = "<tr id=\"playlist_item_#{tracklistindex}\"  class=\"playlist_item #{row_class_addon}\" ondblclick=\"play_from_list(#{tracklistindex})\"><td class=\"artisttd\"><span class=\"artist #{class_addon}\">#{artist}</span></td><td class=\"titletd\"><span class=\"title #{class_addon}\">#{title}</span></td><td class=\"actiontd\"><img onclick=\"delete_from_list(#{tracklistindex})\" src=\"phone/images/down.png\" width=\"16\" height=\"16\" class=\"action\" /></td></tr>";
-var searchresult_item_template_html = "<tr id=\"search_result_#{url}\"  class=\"playlist_item #{row_class_addon}\" ondblclick=\"playElement(#{url})\"><td class=\"artisttd\"><span class=\"artist #{class_addon}\">#{artist}</span></td><td class=\"titletd\"><span class=\"title #{class_addon}\">#{title}</span></td><td class=\"actiontd\"><img onclick=\"collectionPutElementInTrackList(#{url})\" src=\"phone/images/up.png\" width=\"16\" height=\"16\" class=\"action\" /></td></tr>";
-
